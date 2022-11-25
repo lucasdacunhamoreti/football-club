b60f062a3abf55cb0027e434675c0048c023eefa
Lucas da Cunha Moreti <lucasdacunha00@gmail.com>
2022-11-25 17:41:25 -0300

    feat: Implements integration tests

---
+++ app/backend/src/tests/leaderboard.test.ts
@@ -0,0 +1,46 @@
+import * as chai from 'chai';
+import * as mocha from 'mocha';
+// @ts-ignore
+import chaiHttp = require('chai-http')
+// @ts-ignore
+import Sinon = require('sinon')
+import MatchModel from '../database/models/Match';
+import { placing } from './mocks/leaderboard';
+import { matchesNotInProgress } from './mocks/match';
+import { app } from '../app'; 
+import { StatusCodes as code } from 'http-status-codes';
+
+chai.use(chaiHttp);
+
+const { expect, request } = chai;
+
+describe('Integration testing on the /leaderboard endpoint', () => {
+    describe('Lists all scores of team home', () => {
+        before(() => {
+            Sinon.stub(MatchModel, 'findAll').resolves(matchesNotInProgress as any); // Arrumar
+        });
+    
+        after(() => {
+            (MatchModel.findAll as sinon.SinonStub).restore();
+        });
+
+        it('Returns code 200 and a list of scores', async () => {
+            const result = await request(app).get('/leaderboard/home').send();
+            
+            expect(result.body).to.eql(placing);
+            expect(result).to.have.status(code.OK);
+            expect(result.body[0]).to.have.property('name');
+            expect(result.body[0]).to.have.property('totalPoints');
+            expect(result.body[0]).to.have.property('totalGames');
+            expect(result.body[0]).to.have.property('totalVictories');
+            expect(result.body[0]).to.have.property('totalDraws');
+            expect(result.body[0]).to.have.property('totalLosses');
+            expect(result.body[0]).to.have.property('goalsFavor');
+            expect(result.body[0]).to.have.property('goalsOwn');
+            expect(result.body[0]).to.have.property('goalsBalance');
+            expect(result.body[0]).to.have.property('efficiency');
+        });
+    });
+
+
+});
---
+++ app/backend/src/tests/mocks/leaderboard.ts
@@ -0,0 +1,86 @@
+export const placing = [
+    {
+        name: 'Corinthians',
+        totalPoints: 3,
+        totalVictories: 1,
+        totalDraws: 0,
+        totalLosses: 0,
+        goalsFavor: 3,
+        goalsOwn: 0,
+        totalGames: 1,
+        goalsBalance: 3,
+        efficiency: '100.00'
+        },
+        {
+        name: 'Palmeiras',
+        totalPoints: 1,
+        totalVictories: 0,
+        totalDraws: 1,
+        totalLosses: 0,
+        goalsFavor: 2,
+        goalsOwn: 2,
+        totalGames: 1,
+        goalsBalance: 0,
+        efficiency: '33.33'
+        },
+        {
+        name: 'São Paulo',
+        totalPoints: 1,
+        totalVictories: 0,
+        totalDraws: 1,
+        totalLosses: 0,
+        goalsFavor: 1,
+        goalsOwn: 1,
+        totalGames: 1,
+        goalsBalance: 0,
+        efficiency: '33.33'
+        },
+        {
+        name: 'Internacional',
+        totalPoints: 1,
+        totalVictories: 0,
+        totalDraws: 1,
+        totalLosses: 0,
+        goalsFavor: 1,
+        goalsOwn: 1,
+        totalGames: 1,
+        goalsBalance: 0,
+        efficiency: '33.33'
+        },
+        {
+        name: 'Flamengo',
+        totalPoints: 1,
+        totalVictories: 0,
+        totalDraws: 1,
+        totalLosses: 0,
+        goalsFavor: 1,
+        goalsOwn: 1,
+        totalGames: 1,
+        goalsBalance: 0,
+        efficiency: '33.33'
+        },
+        {
+        name: 'Cruzeiro',
+        totalPoints: 1,
+        totalVictories: 0,
+        totalDraws: 1,
+        totalLosses: 0,
+        goalsFavor: 1,
+        goalsOwn: 1,
+        totalGames: 1,
+        goalsBalance: 0,
+        efficiency: '33.33'
+        },
+        {
+        name: 'Botafogo',
+        totalPoints: 1,
+        totalVictories: 0,
+        totalDraws: 1,
+        totalLosses: 0,
+        goalsFavor: 0,
+        goalsOwn: 0,
+        totalGames: 1,
+        goalsBalance: 0,
+        efficiency: '33.33'
+        }
+]
